132799597d7f818bd65edb2bc667ab8ae0138cc0
Digitalova <amadou.738o@gmail.com>
2026-01-18 05:00:05 +0100

    CODEfinal

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
-    unoptimized: true, // Nécessaire pour l'export statique
     remotePatterns: [
       {
         protocol: 'https',
